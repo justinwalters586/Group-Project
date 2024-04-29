fdace8dac120badb8f370c2485f381496fc68c79
WaltersJ07 <WaltersJ07@uww.edu>
2024-04-29 10:06:17 -0500

    ERA

--- ERA.py
+++ ERA.py
@@ -13,14 +13,10 @@ df = pd.merge(pitching_df, people_df, on='playerID')
 df['age'] = df['yearID'] - df['birthYear']
 
 # Filter out records where 'ERA' is 0 and 'age' is not within a reasonable range
-df = df[(df['ERA'] != 0) & (df['age'] >= 20) & (df['age'] <= 38) & (df['IPouts'] > 150)]
+df = df[(df['ERA'] != 0) & (df['age'] >= 20) & (df['age'] <= 40) & (df['IPouts'] > 150)]
 
 # Scatter plot of each player's ERA at their specific age
 plt.scatter(df['age'], df['ERA'], alpha=0.5)
-
-# Calculate the mean ERA for each age and plot it
-mean_eras = df.groupby('age')['ERA'].mean()
-plt.plot(mean_eras.index, mean_eras.values, color='red', linewidth=2)
 
 # Show the plot with labels
 plt.xlabel('Age')
